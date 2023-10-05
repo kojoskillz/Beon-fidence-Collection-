f2c90957b37ae4e73b389508afadc647a2b2763a
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2023-10-05 02:36:51 +0000

    Add files via upload

---
+++ dist/script.js
@@ -0,0 +1,25 @@
+// auto-slide
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+
+function showSlide(slideIndex) {
+  slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+      slide.classList.add('active');
+    } else {
+      slide.classList.remove('active');
+    }
+  });
+}
+
+function nextSlide() {
+  currentSlide++;
+  if (currentSlide === slides.length) {
+    currentSlide = 0;
+  }
+  showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 4000); // Change slide every 3 seconds
+
